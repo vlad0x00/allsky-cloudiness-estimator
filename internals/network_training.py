847d0ab180e1a43ca8ee1fefec55be85df98bbd7
schutzekatze <vladimir.nik94@gmail.com>
2018-03-29 15:37:49 +0200

    Add data shuffle

--- internals/network_training.py
+++ internals/network_training.py
@@ -5,6 +5,7 @@ from os import walk
 from os.path import join
 from neural_network.NeuralNetwork import NeuralNetwork
 from utils.labeling import is_label, remove_label
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('image_dir', help='Directory with train images and their labels')
@@ -22,6 +23,9 @@ def get_image_paths(dir):
         for filename in filenames:
             if is_label(filename):
                 paths.append((join(dirpath, remove_label(filename)), join(dirpath, filename)))
+
+    random.shuffle(paths)
+
     return paths
 
 nn = NeuralNetwork()
